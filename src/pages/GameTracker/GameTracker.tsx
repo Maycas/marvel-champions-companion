48e0db4041fadb32020b83e3b06daff06b1dc75a
Marc Maycas <marc.maycas@gmail.com>
2024-05-08 12:48:18 +0200

    Splitting Character into Villains and Heroes section

--- src/pages/GameTracker/GameTracker.tsx
+++ src/pages/GameTracker/GameTracker.tsx
@@ -1,17 +1,21 @@
 import Container from '../../components/Container/Container';
 import Character from '../../components/Character/Character';
 
+import styles from './GameTracker.module.css';
 
 export default function GameTracker() {
   return (
     <Container direction='column'>
-      <Character name='Rino' lives={14} colors={[
-        '#727070'
-      ]}/>
-      <Character name='Spiderman' lives={10} colors={[
-        "#982828",
-        "#2b3681",
-      ]}/>
+      <div className={styles.villains}>
+        <Character name='Rino' lives={14} colors={['#727070']} />
+      </div>
+      <div className={styles.heroes}>
+        <Character
+          name='Spiderman'
+          lives={10}
+          colors={['#982828', '#2b3681']}
+        />
+      </div>
     </Container>
   );
 }
